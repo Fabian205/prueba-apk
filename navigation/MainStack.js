49d30b0bf27eb6f96cf70142244e642cd3a475eb
Fabian205 <fabaguilar205@gmail.com>
2023-03-07 17:57:53 -0500

    Agrego Screens prueba-apk4

--- navigation/MainStack.js
+++ navigation/MainStack.js
@@ -7,6 +7,9 @@ import LoginScreen from '../screens/LoginScreen';
 import BienvenidosScreen from '../screens/BienvenidosScreen';
 import CambiarContraScreen from '../screens/CambiarContraScreen';
 import MenuInicioScreen from '../screens/MenuInicioScreen';
+import EstadosCuentaScreen from '../screens/EstadosCuentaScreen';
+import ConsultaPagosScreen from '../screens/ConsultaPagosScreen';
+import HistorialPropiedadScreen from '../screens/HistorialPropiedadScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -37,6 +40,18 @@ const MainStack = () => {
                 <Stack.Screen
                     name='MenuInicio'
                     component={MenuInicioScreen}
+                />
+                <Stack.Screen
+                    name='EstadosCuenta'
+                    component={EstadosCuentaScreen}
+                />
+                <Stack.Screen
+                    name='ConsultaPagos'
+                    component={ConsultaPagosScreen}
+                />
+                <Stack.Screen
+                    name='HistorialPropiedad'
+                    component={HistorialPropiedadScreen}
                 />                               
             </Stack.Navigator>           
         </NavigationContainer>
